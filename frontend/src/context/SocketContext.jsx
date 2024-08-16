85f300689553b16b9e71cb441d999369d0a686d3
sonuchahar7351 <sonuchahar7351@gmail.com>
2024-08-16 14:41:26 +0530

    Deployment

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({children}) => {
 
 
       useEffect(()=>{
-            const socket = io("http://localhost:5000",{
+            const socket = io("/",{
                   query:{
                         userId : user?._id
                   }
